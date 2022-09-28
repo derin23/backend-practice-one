edb6fd45031d196501b87dea8b34de50accef349
derin <derinbino@gmail.com>
2022-09-27 20:39:28 -0500

    REST API Time!

---
+++ src/main/java/com/derin/backendpracticeone/BackendPracticeOneApplication.java
@@ -0,0 +1,13 @@
+package com.derin.backendpracticeone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendPracticeOneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendPracticeOneApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/derin/backendpracticeone/BackendPracticeOneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.derin.backendpracticeone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendPracticeOneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
