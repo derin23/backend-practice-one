f525d47a76045f74c2f7711a5c42ce6523042799
derin <derinbino@gmail.com>
2022-09-29 01:36:35 -0500

    added get mapping

--- src/main/java/com/derin/backendpracticeone/controller/UserController.java
+++ src/main/java/com/derin/backendpracticeone/controller/UserController.java
@@ -3,9 +3,9 @@ package com.derin.backendpracticeone.controller;
 import com.derin.backendpracticeone.model.User;
 import com.derin.backendpracticeone.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 public class UserController {
@@ -16,4 +16,17 @@ public class UserController {
     User newUser(@RequestBody User newUser){
         return userRepository.save(newUser);
     }
+
+    @GetMapping("/users")
+    List<User> getAllUsers(){
+        return userRepository.findAll();
+    }
+//    @GetMapping("/users/{id}")
+//    public User getUser(@PathVariable(value = "id") Long id) {
+//
+//        Iterable<Integer> result = id.stream()
+//                .map(Long::intValue)
+//                .collect(Collectors.toList());
+//        return userRepository.findAllById(result);
+//    }
 }
