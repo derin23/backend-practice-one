80b4a188ba57ee5438465ee947e17823de81401a
derin <derinbino@gmail.com>
2022-10-24 01:44:01 -0500

    fun!

--- src/main/java/com/derin/backendpracticeone/controller/UserController.java
+++ src/main/java/com/derin/backendpracticeone/controller/UserController.java
@@ -39,6 +39,7 @@ public class UserController {
         }).orElseThrow(()-> new UserNotFoundException(id));
     }
 
+
     @DeleteMapping("/user/{id}")
     String deleteUser(@PathVariable Long id){
         if(!userRepository.existsById(id)){
